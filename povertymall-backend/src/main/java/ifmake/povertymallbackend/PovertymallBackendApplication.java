e9083c61039b5374ab51ac95447facf2624ff3ad
wch <1325857420@qq.com>
2019-07-08 23:52:19 +0800

    init

---
+++ povertymall-backend/src/main/java/ifmake/povertymallbackend/PovertymallBackendApplication.java
@@ -0,0 +1,13 @@
+package ifmake.povertymallbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PovertymallBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PovertymallBackendApplication.class, args);
+	}
+
+}
---
+++ povertymall-backend/src/test/java/ifmake/povertymallbackend/PovertymallBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package ifmake.povertymallbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PovertymallBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
